a718d8e084aec7ec86ae7ca3df7ea07065467dcb
roschly <roschly@gmail.com>
2021-10-10 13:13:52 +0200

    show all folders, files and classes with different shapes

--- classuml/__main__.py
+++ classuml/__main__.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import List
+from typing import Dict, List, Tuple
 
 import astroid
 import pydot
@@ -43,35 +43,65 @@ def main():
         (path.relative_to(root.parent), module) for path, module in path_and_modules
     ]
 
-    # [
-    #     print(
-    #         path.relative_to(root), ClassExtractor(filepath=path, module=module).classes
-    #     )
-    #     for path, module in path_and_modules
-    # ]
-
     dot = pydot.Dot(
-        graph_type="digraph", rankdir="LT", fontname="Helvetica", nodesep=2, ranksep=2
+        graph_type="digraph", rankdir="LR", fontname="Helvetica", nodesep=1, ranksep=1
     )
     dot.set_node_defaults(fontname="Helvetica")
+
+    # handle nodes and edges in dicts, to avoid pydots double quoting "" of node names
+    # which makes it very difficult to locate the proper node from path (via pydot)
+    nodes: Dict[Path, pydot.Node] = {}
+    edges: Dict[Tuple[Path, Path], pydot.Edge] = {}
+
+    # add root as first node
+    nodes[root.relative_to(root.parent)] = pydot.Node(name=root.stem)
+
+    # add all files and folders as nodes
+    for filepath, _ in relative_paths_and_modules:
+        path = filepath
+        for i in range(len(filepath.parts) - 1):
+            if not path in nodes:
+                nodes[path] = pydot.Node(name=path.as_posix())
+            path = path.parent
+
+    # add directory hierarchy between folders and files, as edges
+    for filepath, _ in relative_paths_and_modules:
+        path = filepath
+        for i in range(len(filepath.parts) - 1):
+            parent = path.parent
+            edge = pydot.Edge(nodes[parent], nodes[path])
+            if not (parent, path) in edges:
+                edges[(parent, path)] = edge
+            path = parent
+
+    # if _is_package(network=network, node=node): self.set("shape", "component")
+    #     else: self.set("shape", "folder")
+    #     self.set("color", "red")
+
+    # add nodes to pydot graph
+    for path, node in nodes.items():
+        shape = "folder" if path.is_dir() else "component"
+        node.set("shape", shape)
+        dot.add_node(node)
+
+    # add edges to pydot graph
+    for _, edge in edges.items():
+        dot.add_edge(edge)
+
+    # add edge from file/module to class
     for path, module in relative_paths_and_modules:
-        parent_node = pydot.Node(name=path.parent.as_posix())
-        dot.add_node(parent_node)
-        path_node = pydot.Node(name=path.as_posix())
-        dot.add_node(path_node)
-
-        dot.add_edge(pydot.Edge(parent_node, path.as_posix()))
-
         cls_extractor = ClassExtractor(filepath=path, module=module)
         for cls in cls_extractor.classes:
             qualified_class_name = cls.name
+            path_node = nodes[path]
             cls_node = pydot.Node(name=qualified_class_name)
+            cls_node.set("shape", "rect")
             dot.add_node(cls_node)
             dot.add_edge(pydot.Edge(path_node, cls_node))
 
     # output as string or image file
     dot.write("bla.png", prog="dot", format="png")
-    # print(dot.to_string())
+    print(dot.to_string())
 
 
 if __name__ == "__main__":
